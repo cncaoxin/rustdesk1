72c882294018a2f3838dba592ea7d1cd5b4efc83
cncaoxin <108396788+cncaoxin@users.noreply.github.com>
2025-05-22 09:56:58 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -993,7 +993,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://rustdesk.641221.xyz:21114".to_owned()
 }
 
 #[inline]
